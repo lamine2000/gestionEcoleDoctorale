cabb59955ce8a75fd4694aa592362c0eadef2778
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2021-11-09 11:50:37 +0000

    ajout de code (directeur de thèse)

--- src/app/user-views/directeur-these/directeur-these.component.ts
+++ src/app/user-views/directeur-these/directeur-these.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Doctorant} from "../../models/doctorant";
+import {Subscription} from "rxjs";
+import {DoctorantsService} from "../../services/doctorants.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-directeur-these',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirecteurTheseComponent implements OnInit {
 
-  constructor() { }
+  demandesEnAttente: Doctorant[] = [];
+  doctorants: Doctorant[] = [];
+  doctorantsSubscription! : Subscription;
+
+  constructor(
+    private doctorantsService: DoctorantsService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.doctorantsSubscription = this.doctorantsService.doctorantsSubject.subscribe(
+      (doctorants) => {
+        this.doctorants = doctorants;
+      }
+    );
+    this.doctorantsService.emitDoctorants();
   }
 
+  viewDemandes(){
+    this.demandesEnAttente = [];
+    for (const elt of this.doctorants) {
+      if(!elt.compteActif /*&& elt.etape == 1*/){
+        this.demandesEnAttente.push(elt);
+      }
+    }
+  }
+
+  onViewDoctorant(i: number){
+    let realIndex: number = this.doctorants.indexOf(this.demandesEnAttente[i]);
+    this.router.navigate(['/doctorants', 'single', realIndex]);
+  }
+
+  ngOnDestroy(){
+    this.doctorantsSubscription.unsubscribe();
+  }
 }
--- src/app/user-views/secretaire-edmi/secretaire-edmi.component.ts
+++ src/app/user-views/secretaire-edmi/secretaire-edmi.component.ts
@@ -32,7 +32,7 @@ export class SecretaireEdmiComponent implements OnInit {
   viewDemandes(){
     this.demandesEnAttente = [];
     for (const elt of this.doctorants) {
-      if(!elt.compteActif /*&& elt.etape == 1*/){
+      if(elt.etape == 2){
         this.demandesEnAttente.push(elt);
       }
     }
@@ -41,6 +41,10 @@ export class SecretaireEdmiComponent implements OnInit {
   onViewDoctorant(i: number){
     let realIndex: number = this.doctorants.indexOf(this.demandesEnAttente[i]);
     this.router.navigate(['/doctorants', 'single', realIndex]);
+  }
+
+  ngOnDestroy(){
+    this.doctorantsSubscription.unsubscribe();
   }
 
 }
