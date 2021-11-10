08574b13a3ef1889ca52bd2d8442a2bd2eba6279
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2021-11-10 18:26:57 +0000

    doctorant user done !

--- src/app/user-views/doctorant/doctorant.component.ts
+++ src/app/user-views/doctorant/doctorant.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router } from '@angular/router';
+import {Doctorant} from "../../models/doctorant";
+import {DoctorantsService} from "../../services/doctorants.service";
 
 @Component({
   selector: 'app-doctorant',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorantComponent implements OnInit {
 
-  constructor() { }
+  doctorant!: Doctorant;
+  steps: string[] =
+    [
+      'directer-these',
+      'secretaire-edmi',
+      'directeur-laboratoire',
+      'responsable-formation-doctorale',
+      'directeur-ecole-doctorale'
+    ]
+  i!: number;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private doctorantsService: DoctorantsService
+  ) { }
 
   ngOnInit(): void {
+    this.i = this.route.snapshot.params['id'];
+    this.doctorantsService.getSingleDoctorant(+this.i).then(
+      (doctorant) => {this.doctorant = doctorant;}
+    );
+  }
+
+  getStyle(step: string){
+    let etape = this.steps.indexOf(step)+1;
+    if(this.doctorant.etape == etape){
+      return 'orange';
+    }
+    else if(this.doctorant.etape > etape){
+      return 'green';
+    }
+    else{
+      return 'red';
+    }
   }
 
 }
